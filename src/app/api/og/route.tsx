e39ff3e95bc650c2392297746230d5a46d183a43
jhangmez <jhangomez25@gmail.com>
2024-02-20 12:16:55 -0500

    Se cambiara el nombre del repositorio a quatu, aun sigue con el error

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -109,7 +109,6 @@ export async function GET(request: Request) {
       }
     )
   } catch (e: any) {
-    // console.log(`${e.message}`)
     return new Response(`Failed to generate the image`, {
       status: 500
     })
