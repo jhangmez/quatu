723acd7bf5236c993442ab132d09112cb29872c3
jhangmez <jhangomez25@gmail.com>
2024-02-21 16:18:02 -0500

    Se actualizo el uploaderthing y se movio a otro lado

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,10 @@ import { Analytics } from '@vercel/analytics/react'
 import { Toaster } from 'react-hot-toast'
 import { Suspense } from 'react'
 import Loading from './loading'
+import { NextSSRPlugin } from '@uploadthing/react/next-ssr-plugin'
+import { extractRouterConfig } from 'uploadthing/server'
+
+import { uploadRouter } from '@api/uploadthing/core'
 
 const myFont = localFont({ src: './LexendDeca-VariableFont_wght.ttf' })
 
@@ -14,13 +18,14 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang='en'>
+    <html lang='es'>
       <body
         className={`${myFont.className} selection:bg-light-primary selection:text-light-onPrimary dark:selection:bg-dark-primary dark:selection:text-dark-onPrimary`}
       >
+        <NextSSRPlugin routerConfig={extractRouterConfig(uploadRouter)} />
         <Providers>
           <Toaster />
-          <noscript>Página realizada por @jhangmez de HarkaySoft s</noscript>
+          <noscript>Página realizada por @jhangmez de HarkaySoft</noscript>
           <Suspense fallback={<Loading />}>{children}</Suspense>
           <Analytics />
         </Providers>
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,25 +5,11 @@ import { ApolloWrapper } from '@lib/graphql'
 import { NextUIProvider } from '@nextui-org/react'
 import { useRouter } from 'next/navigation'
 
-import { NextSSRPlugin } from '@uploadthing/react/next-ssr-plugin'
-import { extractRouterConfig } from 'uploadthing/server'
-
-import { uploadRouter } from '@api/uploadthing/core'
-
 export function Providers({ children }: { children: React.ReactNode }) {
   const router = useRouter()
   return (
     <SessionProvider>
       <ApolloWrapper>
-        <NextSSRPlugin
-          /**
-           * The `extractRouterConfig` will extract **only** the route configs
-           * from the router to prevent additional information from being
-           * leaked to the client. The data passed to the client is the same
-           * as if you were to fetch `/api/uploadthing` directly.
-           */
-          routerConfig={extractRouterConfig(uploadRouter)}
-        />
         <NextUIProvider navigate={router.push}>{children}</NextUIProvider>
       </ApolloWrapper>
     </SessionProvider>
